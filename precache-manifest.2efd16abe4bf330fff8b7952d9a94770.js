2fb3f5096d37ef4c940475cbbb4b0cf10a52f448
Suno <dev.suno01@gmail.com>
2020-10-26 11:05:05 +0800

    deploy

---
+++ precache-manifest.2efd16abe4bf330fff8b7952d9a94770.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca7eb8bba2992d7d5f5",
+    "url": "/moze-web/css/app.dc9797d4.css"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "/moze-web/css/chunk-vendors.c2c417c0.css"
+  },
+  {
+    "revision": "312575c00866228a7250ae1a38eaa0d1",
+    "url": "/moze-web/index.html"
+  },
+  {
+    "revision": "eca7eb8bba2992d7d5f5",
+    "url": "/moze-web/js/app.7a1229d9.js"
+  },
+  {
+    "revision": "94e03ba2c1b608ed9d00",
+    "url": "/moze-web/js/chunk-vendors.23165292.js"
+  },
+  {
+    "revision": "fc17eb22641c4a00df04e5e15d6988b3",
+    "url": "/moze-web/manifest.json"
+  },
+  {
+    "revision": "c533c26c9ecdad3aabcd124d5f8c4a82",
+    "url": "/moze-web/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moze-web/robots.txt"
+  }
+]);
